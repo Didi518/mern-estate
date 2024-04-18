b045ad6f03e7f21618e483060d53c2ec6501182b
Didi518 <kevin.ouali@gmail.com>
2024-04-18 15:45:10 +0200

    :page_facing_up: create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 export default function () {
-  return <h1 className="text-red-500">App</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/connexion" element={<SignIn />} />
+        <Route path="/inscription" element={<SignUp />} />
+        <Route path="/presentation" element={<About />} />
+        <Route path="/profil" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
